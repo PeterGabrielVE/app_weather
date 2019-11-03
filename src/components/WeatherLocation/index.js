8d1840acef209062e0d0248ea2409f6d3d813586
unknown <gaboleal123@gmail.com>
2019-11-03 11:10:55 -0430

    api_weather

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -7,6 +7,14 @@ import {
     WINDY,
 } from './../../constants/weathers';
 
+const location = "Barquisimeto,VE";
+const api_key = "6e4ea37823111c23ec8eb9dbe1f87f1f";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
+
+
 const data = {
     temperature: 24,
     weatherState: SUN,
@@ -34,9 +42,12 @@ class WeatherLocation extends Component {
     }
 
     handleUpdateClick = () => {
+
+        fetch(api_weather);
+
         console.log('update');
+
         this.setState({
-            city: 'Cabudare',
             data:data2,
         });
     }
