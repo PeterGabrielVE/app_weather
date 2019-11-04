960325b8965eb3720f001fe912933255956b350a
unknown <gaboleal123@gmail.com>
2019-11-04 10:41:37 -0430

    LocationList

---
+++ src/components/LocationList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WheaterLocation from './WeatherLocation';
+
+const LocationList = () => (
+    <div>
+        <WheaterLocation city="Barquisimeto,VE"></WheaterLocation>
+        <WheaterLocation city="Caracas,VE"></WheaterLocation>
+        <WheaterLocation city="Valencia,VE"></WheaterLocation>
+    </div>
+)
+
+export default LocationList;
