fd6e40f9bad86f77896d3f3df62b01c35d2492e9
Pedro Gabriel <gaboleal123@gmail.com>
2019-11-13 13:26:29 -0430

    pronostico extendido

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,30 +1,67 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
+import transformForecast from './../services/transformForecast';
 import './styles.css';
 
-const days = [
+/*const days = [
     'Lunes',
     'Martes',
     'Miercoles',
     'Jueves',
     'Viernes',
-];
+];*/
 
+const url = "http://api.openweathermap.org/data/2.5/forecast";
+const  api_key= "6e4ea37823111c23ec8eb9dbe1f87f1f";
 
 class ForecastExtended extends Component {
 
-    renderForecastItemDays(){
-        return days.map( day =>  (<ForecastItem weekDay={day}></ForecastItem>));
+    constructor(){
+        super();
+        this.state = { forecastData: null }
+    }
+
+    componentDidMount(){
+        const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`;
+
+        fetch(url_forecast).then(
+            data => (data.json())
+        ).then(
+            weather_data => {
+                console.log(weather_data);
+                const forecastData = transformForecast(weather_data);
+                console.log(forecastData);
+                this.setState({ forecastData });
+            }
+        );
+    }
+
+    renderForecastItemDays(forecastData){
+        //return <h1>Render Items</h1>;
+        return forecastData.map( forecast => 
+            (<ForecastItem 
+                key={`${forecast.weekDay}${forecast.hour}`}
+                weekDay={forecast.weekDay}
+                hour={forecast.hour}
+                data={forecast.data}>
+            </ForecastItem>));
+    }
+
+    renderProgress = ()=>{
+        return <h3>Cargando pronóstico extendido..</h3>;
     }
 
 
     render(){
         const { city } = this.props;
+        const { forecastData } = this.state;
+
         return(
             <div className="forecast-title">
                 <h2>Pronóstico extendido para {city}</h2>
-                {this.renderForecastItemDays()}
+                { forecastData ? this.renderForecastItemDays(forecastData) :
+                this.renderProgress()}
             </div>
         );
     }
---
+++ src/services/transformForecast.js
@@ -0,0 +1,24 @@
+import moment from 'moment';
+import 'moment/locale/es';
+import transformWeather from './transformWeather';
+
+
+
+const transformForecast = data => (
+    data.list.filter(item => {
+        const hour = moment.utc(moment.unix(item.dt)).hour();
+        return (
+            hour === 6 ||
+            hour === 12 ||
+            hour === 18
+        );
+    }).map(item => (
+        {
+            weekDay: moment.unix(item.dt).format('ddd'),
+            hour: moment.unix(item.dt).hour(),
+            data: transformWeather(item)
+        }
+    ))
+);
+
+export default transformForecast;
