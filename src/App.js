60d05b2eddb91792d5a792249de0f62b63b71c3a
Pedro Gabriel <gaboleal123@gmail.com>
2019-11-14 11:36:23 -0430

    action creator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import { createStore } from 'redux';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
@@ -15,6 +16,11 @@ const cities = [ 'Barquisimeto,ve',
                   'Barcelona,es',
                   'Madrid,es'];
 
+const store  = createStore( () => {},
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );                 
+
+const setCity = value => ({ type:'setType', value});
+
 class App extends Component {
 
     constructor(){
@@ -25,6 +31,9 @@ class App extends Component {
     handleSelectedLocation = city =>{
         this.setState({city});
         console.log("handleSelectionLocation");
+
+        
+        store.dispatch(setCity(city));
     }
     render(){
         const { city } = this.state;
